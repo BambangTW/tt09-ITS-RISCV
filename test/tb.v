52a408b6d275a6e86aa93fd18f0beb3b071a21b0
BAMBANG TRIARTHO WIBOWO <bambangwibowo.205022@mhs.its.ac.id>
2024-11-04 14:10:35 +0700

    adding GL_TEST

--- test/tb.v
+++ test/tb.v
@@ -27,16 +27,19 @@ module tb ();
    wire	     gpio2;
    wire	     gpio3;
    wire	     gpio4;
-   
+`ifdef GL_TEST
+    wire VPWR = 1'b1;
+    wire VGND = 1'b0;
+`endif
 
   // Replace tt_um_example with your module name:
   tt_um_underserved user_project (
 
       // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
+      `ifdef GL_TEST
+            .VPWR(VPWR),
+            .VGND(VGND),
+      `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out ({uo_out[7:5], gpio4, gpio3, gpio2, gpio1, gpio0}),   // Dedicated outputs
